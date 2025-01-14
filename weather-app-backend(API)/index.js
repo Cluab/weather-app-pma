cb34a732a715c620d45f4555ff8c502e96323857
cluab <ebra.alyousefi@gmail.com>
2025-01-14 07:10:34 -0800

    Refactor API routes and

--- weather-app-backend(API)/index.js
+++ weather-app-backend(API)/index.js
@@ -6,6 +6,9 @@ const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const createRoute = require('./routes/api/create');
+const readRoute = require('./routes/api/read');
+const updateRoute = require('./routes/api/update');
+const deleteRoute = require('./routes/api/delete');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -23,10 +26,9 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/create', createRoute);
-console.log('Create route loaded');
 
-// app.use('/api/read', readRoute);
-// app.use('/api/update', updateRoute);
-// app.use('/api/delete', deleteRoute);
+app.use('/api/read', readRoute);
+app.use('/api/update', updateRoute);
+app.use('/api/delete', deleteRoute);
 
 connectDB();
