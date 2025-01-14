a4403a43952877665ff4c5e0dd648d659a6134dc
Ebrahim Suhail Al-Yousefi <ebra.alyousefi@gmail.com>
2025-01-13 16:38:55 -0800

    Update AutocompleteInput.jsx

--- weather-app-pma/src/components/AutocompleteInput.jsx
+++ weather-app-pma/src/components/AutocompleteInput.jsx
@@ -9,7 +9,7 @@ const libraries = ["places"]; // Add 'places' library for Places API
 
 const AutocompleteInput = ({ onLocationSelect }) => {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyAkbv-fe1CQonV-4ttMRuvi6r-2iNnTpQs", // Replace with your API key
+    googleMapsApiKey: "", // Replace with your API key
     libraries,
   });
 
